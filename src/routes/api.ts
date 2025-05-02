70633d845c1c264c593bb80ca5c1afe5b2866f39
eggy <eggy.atma@zicare.id>
2025-05-02 08:56:13 +0700

    feat: init project be-event

---
+++ src/routes/api.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import dummyController from "../controllers/dummy.controllers";
+
+const router = express.Router();
+
+router.get('/dummy', dummyController.dummy);
+
+export default router;
